a7a86494807df157bcc8b2302f08ed7bfb2295c3
fujis <ppdx999@gmail.com>
2024-11-15 17:27:12 +0900

    fixup build.sh

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ timestamp=$(date '+%Y%m%d%H%M%S')
 cmdname=$(basename $0)
 tmp=/tmp/$cmdname.$timestamp.$$
 
-cat html/hello.py |
+cat src/hello.py |
 sed 's/^/<td class="code-content"><code>/' |
 sed 's;$;</code></td></tr>;' |
 awk '{print "<tr><td class=\"line-number\"><code>" NR "</code></td>" $0}' > $tmp-hello.py
 
-cat html/index.html |
-filehame -l___CODE___ - $tmp-hello.py > html/test.html
+cat src/index.html |
+filehame -l___CODE___ - $tmp-hello.py > src/test.html
